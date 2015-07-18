6edb97f8d072cc7e000fb60ce02d79de691002eb
Paulin Trognon <paulin.trognon@gmail.com>
2015-07-18 17:40:11 +0200

    v3

--- textes/portrait.txt.php
+++ textes/portrait.txt.php
@@ -3,15 +3,15 @@
     <img src="http://www.sophiemasson.com/images/portrait_sophie_masson_porcelain.jpg" alt="Portrait de Sophie Masson, créatrice de vaisselle en porcelaine peinte à la main." />
 
     <p class="portrait-text">
-        Styliste formée à Paris au cours Bercot, Sophie parfait<br />
+        Styliste formée à Paris au cours Bercot, Sophie parfait
         son expérience dans les bureaux de tendances parisiens.<br />
-        En parallèle, elle décide de suivre une formation de décoratrice sur porcelaine.<br/>
-        Passionée, elle se perfectionne à Limoges et s'affirme<br />
+        En parallèle, elle décide de suivre une formation de décoratrice sur porcelaine.
+        Passionée, elle se perfectionne à Limoges et s'affirme
         auprès des professesionnels et particuliers.<br />
         Sa créativité est vite repérée par les décorateurs et journalistes.<br />
-        Elle illustre des sujets pour la presse déco, crée avec ses clients des services<br />
-        sur mesure et propose aux boutiques et décorateurs des collections exclusives<br />
-        ainsi que des projets évènementiels Lancôme, Réunion des musées nationaux,<br />
+        Elle illustre des sujets pour la presse déco, crée avec ses clients des services
+        sur mesure et propose aux boutiques et décorateurs des collections exclusives
+        ainsi que des projets évènementiels Lancôme, Réunion des musées nationaux,
         les Fées d'Herbe, Elle déco...
     </p>
 </div>
@@ -21,13 +21,13 @@
     <img src="http://www.sophiemasson.com/images/portrait_valerie_delobal.jpg" alt="Portrait de Sophie Masson, créatrice de vaisselle en porcelaine peinte à la main." />
     
     <p class="portrait-text">
-        Depuis sa plus tendre enfance, Valérie parcourt le monde<br />
+        Depuis sa plus tendre enfance, Valérie parcourt le monde
         à la recherche du beau.<br />
         Des rencontres, des voyages inspirants ont peaufiné son sens de l'esthétisme.<br />
-        Après un passage dans le monde de la mode, et toujours avide de<br />
+        Après un passage dans le monde de la mode, et toujours avide de
         nouveautés, Valérie s'affirme dans le domaine de la communication.<br />
-        Devenue Directeur Artistique en Agence,<br />
-        Valérie collabore avec des grands noms de la déco:<br />
+        Devenue Directeur Artistique en Agence,
+        Valérie collabore avec des grands noms de la déco:
         Gilles de Chabaneix, Patrick Vanrobays, Véronique Mery, Christian Lacroix...
     </p>
 </div>
